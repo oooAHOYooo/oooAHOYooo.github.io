082caae6460536b7d8626b8d93be9a6fda12e43e
Ahoy Indie Media <112486736+oooAHOYooo@users.noreply.github.com>
2023-04-24 21:40:51 -0400

    Update marketData.js

--- marketData.js
+++ marketData.js
@@ -5,8 +5,9 @@ var marketplace = new Vue({
         { id: 1, name: 'Pal-bot 2 (VHS)', price: 28, description: 'This is a short description of product 1', image: 'https://via.placeholder.com/150' },
         { id: 2, name: 'Product 2', price: 20, description: 'This is a short description of product 2', image: 'https://via.placeholder.com/150' },
         { id: 3, name: 'Pal-bot 2 Sticker Series', price: 30, description: 'This is a short description of product 3', image: 'https://via.placeholder.com/150' },
-        { id: 4, name: 'Product 4', price: 40, description: 'This is a short description of product 4', image: 'https://via.placeholder.com/150' },
-        { id: 5, name: 'Product 5', price: 32, description: 'This is a short description of product 5', image: 'https://via.placeholder.com/150' }
+        { id: 4, name: 'Tallboyz Series', price: 40, description: 'This is a short description of product 4', image: 'https://via.placeholder.com/150' },
+        { id: 5, name: 'The Tines Album', price: 32, description: 'This is a short description of product 5', image: 'https://via.placeholder.com/150' },
+        { id: 6, name: 'Rambles and Rough Drafts', price: 22, description: 'This is a short description of product 5', image: 'https://via.placeholder.com/150' }
       ],
       cartItems: [],
     },
