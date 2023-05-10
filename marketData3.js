c5e3ab84f09709dba9fa1b5bbbbd7ddb7b33b722
AhoyIndieMedia <ahoy.dev.dev@gmail.com>
2023-05-10 12:35:23 -0400

    Added the MarketPlace working with the vue data

--- marketData3.js
+++ marketData3.js
@@ -7,9 +7,65 @@ var ahoyMarketplace = new Vue({
                 name: 'Ahoy Defaultz Stickers',
                 description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
                 price: 10.99,
-                image: 'https://via.placeholder.com/150',
+                image: 'https://via.placeholder.com/300',
                 stripeBuyUrl: 'https://buy.stripe.com/test_cN26rKb2B64jaYM144'
             },
+            {
+                id: 2,
+                name: 'Ahoy Slicktz Stickers',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+                price: 120.99,
+                image: 'https://via.placeholder.com/300',
+                stripeBuyUrl: 'https://buy.stripe.com/test_cN26rKb2B64jaYM144'
+            },
+            {
+                id: 3,
+                name: 'Ahoy ASZ',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+                price: 132.99,
+                image: 'https://via.placeholder.com/300',
+                stripeBuyUrl: 'https://buy.stripe.com/test_cN26rKb2B64jaYM144'
+            },
+            {
+                id: 4,
+                name: 'Ahoy ASZ',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+                price: 132.99,
+                image: 'https://via.placeholder.com/300',
+                stripeBuyUrl: 'https://buy.stripe.com/test_cN26rKb2B64jaYM144'
+            },
+            {
+                id: 5,
+                name: 'Ahoy ASZ',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+                price: 132.99,
+                image: 'https://via.placeholder.com/300',
+                stripeBuyUrl: 'https://buy.stripe.com/test_cN26rKb2B64jaYM144'
+            },
+            {
+                id: 6,
+                name: 'Ahoy ASZ',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+                price: 132.99,
+                image: 'https://via.placeholder.com/300',
+                stripeBuyUrl: 'https://buy.stripe.com/test_cN26rKb2B64jaYM144'
+            },
+            {
+                id: 7,
+                name: 'Ahoy ASZ',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+                price: 132.99,
+                image: 'https://via.placeholder.com/300',
+                stripeBuyUrl: 'https://buy.stripe.com/test_cN26rKb2B64jaYM144'
+            },
+            {
+                id: 8,
+                name: 'Ahoy ASZ',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+                price: 132.99,
+                image: 'https://via.placeholder.com/300',
+                stripeBuyUrl: 'https://buy.stripe.com/test_cN26rKb2B64jaYM144'
+            }
             // other products omitted for brevity
         ]
     },
