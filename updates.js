c71b46aadd988e1c75f97d56bb56afbd80820e67
AhoyIndieMedia <ahoy.dev.dev@gmail.com>
2023-05-13 12:42:07 -0400

    Tuned up the updates feed a bit - worked on some max heights and scrolls - brought back the sub-footer - made the toggle button work smoother - improved styles + elimanted previous static code - big push

--- updates.js
+++ updates.js
@@ -5,29 +5,30 @@ var ahoyUpdates = new Vue({
         {
           displayText: "May 2023",
           updates: [
+            { image: 'https://via.placeholder.com/300' },
             { text: "Working on", bold: true },
             { text: "1. The Marketplace functioning (custom baby)", },
             {
               text:
                 '2. Live Shows - up and running - but debating if we should really even have "live shows" instead would people be down for "Yesterday shows?"',
-              image: 'https://via.placeholder.com/300'
+              
             },
             { text: "Pumped about", bold: true },
             {
               text:
                 "Met some great artists at Gather - going to put their music on the radio - Riddle M + Justin Arena",
-              image: 'https://via.placeholder.com/300'
+              
             },
             { text: "Special News", bold: true },
-            { text: 'Ellen Martin joins the party as the radio "Station Manager"', image: 'https://via.placeholder.com/300' }
+            { text: 'Ellen Martin joins the party as the radio "Station Manager"', }
           ]
         },
         {
           displayText: "April 2023",
           updates: [
-            { text: "Began Development on New Web Application", image: 'https://via.placeholder.com/300' },
-            { text: "New Rob Show Episodes Planned", image: 'https://via.placeholder.com/300' },
-            { text: "got the radio working", image: 'https://via.placeholder.com/300' }
+            { text: "Began Development on New Web Application"},
+            { text: "New Rob Show Episodes Planned" },
+            { text: "got the radio working" }
           ]
         }
       ]
