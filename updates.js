8731270c7d6f0f561b119c847e03a08d14b90a03
AhoyIndieMedia <ahoy.dev.dev@gmail.com>
2023-05-17 12:46:23 -0400

    added a favicon - updated font sizes for updates

--- updates.js
+++ updates.js
@@ -5,12 +5,12 @@ var ahoyUpdates = new Vue({
       {
         displayText: "May 2023",
         updates: [
-          { text: "!NEW ROB SHOW EPISODES ARE OUT NOW!", superBold: true },
+          { text: "NEW ROB SHOW EPISODE OUT NOW!", superBold: true },
           { text: "Rob Show Season 2 - Episode 1 is out Wednesday May 17th - with Guest Tyler and Dave Gunn of the Tallboyz" },
           { image: './images/Ahoy-Indie-Media---The-Rob-Show---Season-2---Episode-1---2023.jpg' },
           { text: "Working on", bold: true },
           { text: "Getting The Marketplace functioning Right" },
-          { text: "Getting the Radio to work on all devices" },
+          { text: "Getting the App to work on all screen sizes and all devices - but it's a pain in the booty - we at ahoy appreciate the patience." },
           {
             text:
               '2. Live Shows - up and running - but debating if we should really even have "live shows" instead would people be down for "Yesterday shows?"',
