2d235f2f51bb0469e73ad836d8c91c956d93b096
Alex Gonzalez <agonzalez@agonzalez.local>
2023-06-29 22:28:22 -0400

    update the audio player a bit on the podcast page - added it to the bottom of the page - trying to make it player like

---
+++ podcasts.js
@@ -0,0 +1,15 @@
+
+  var ahoyPodcasts = new Vue({
+    el: '#app',
+    data: {
+      episodes: [
+        {
+          imageUrl: 'episode1.jpg',
+          title: 'Yerr Episode 1: Title',
+          description: 'Description of Episode 1.'
+        },
+        // Add more episodes here based on the provided Vue data
+      ]
+    }
+  });
+
