f38b6d6e283617cae67d634e1f5a99af849ee61a
agBabbbyCakes (ApeWorX, LTD) <alex@apeworx.io>
2023-10-04 11:40:49 -0400

    added a basic sign in page

---
+++ coreScript.js
@@ -0,0 +1,44 @@
+var firebaseConfig = {
+	apiKey: "YOUR_API_KEY",
+	authDomain: "YOUR_AUTH_DOMAIN",
+	projectId: "YOUR_PROJECT_ID",
+	storageBucket: "YOUR_STORAGE_BUCKET",
+	messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+	appId: "YOUR_APP_ID"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+
+  var db = firebase.firestore();
+var provider = new firebase.auth.GoogleAuthProvider();
+
+// Sign-in function
+function signInWithGoogle() {
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+        var user = result.user;
+        console.log("User signed in: ", user);
+    }).catch(function(error) {
+        console.error("Error during sign in: ", error);
+    });
+}
+
+// Function to add playlist for a user
+function addPlaylistForUser(userId, playlistName, songs) {
+    db.collection("users").doc(userId).collection("playlists").add({
+        name: playlistName,
+        songs: songs
+    }).then(function() {
+        console.log("Playlist added!");
+    }).catch(function(error) {
+        console.error("Error adding playlist: ", error);
+    });
+}
+
+// Function to retrieve playlists for a user
+function getPlaylistsForUser(userId) {
+    db.collection("users").doc(userId).collection("playlists").get().then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+            console.log(doc.id, " => ", doc.data());
+        });
+    });
+}
