ce01c8bbf7361a05a037964cdaa5812b3d1f05c4
agBabbbyCakes (ApeWorX, LTD) <alex@apeworx.io>
2023-10-10 18:30:59 -0400

    added basic func

--- library/l4.js
+++ library/l4.js
@@ -1,6 +1,7 @@
-let songs = [];
-
 let currentSongIndex = 0;
+let songs = [];
+let currentPlaylist = [];
+
 const audioPlayer = document.getElementById('audioPlayer');
 const playPauseButton = document.getElementById('playPauseButton');
 
@@ -19,31 +20,18 @@ function playSongFromList(index) {
     audioPlayer.src = songs[currentSongIndex].mp3url;
     audioPlayer.play();
     playPauseButton.innerText = "Pause";
+    document.getElementById('currentArtist').textContent = songs[currentSongIndex].artist || "";
+    document.getElementById('currentSong').textContent = songs[currentSongIndex].songTitle || songs[currentSongIndex].title || "";
 }
 
 function nextSong() {
     currentSongIndex = (currentSongIndex + 1) % songs.length;
-    audioPlayer.src = songs[currentSongIndex].mp3url;
-    audioPlayer.play();
+    playSongFromList(currentSongIndex);
 }
 
 function previousSong() {
     currentSongIndex = (currentSongIndex - 1 + songs.length) % songs.length;
-    audioPlayer.src = songs[currentSongIndex].mp3url;
-    audioPlayer.play();
-}
-
-function addToPlaylist(index) {
-    console.log(`Add Song ${index + 1} to Playlist`);
-}
-
-function addToQueue(index) {
-    console.log(`Add Song ${index + 1} to Queue`);
-}
-
-function likeSong(index) {
-    songs[index].liked = !songs[index].liked;
-    populateSongs();
+    playSongFromList(currentSongIndex);
 }
 
 function adjustVolume() {
@@ -51,32 +39,66 @@ function adjustVolume() {
     audioPlayer.volume = volume;
 }
 
+function updateProgressBar() {
+    const progressBar = document.querySelector("#progressBar div");
+    const percentage = (audioPlayer.currentTime / audioPlayer.duration) * 100;
+    progressBar.style.width = percentage + "%";
+}
+
 function populateSongs() {
     const songList = document.getElementById('songList');
     songList.innerHTML = '';
-
     songs.forEach((song, index) => {
-        const songRow = document.createElement('tr');
-        if (song.artist && song.songTitle) {
-            songRow.innerHTML = `
+        const songRow = `
+            <tr>
                 <td>${song.artist}</td>
                 <td>${song.songTitle}</td>
                 <td><button onclick="playSongFromList(${index})">Play</button></td>
                 <td><button onclick="addToPlaylist(${index})">Add to Playlist</button></td>
                 <td><button onclick="addToQueue(${index})">Add to Queue</button></td>
-                <td><button onclick="likeSong(${index})">${song.play ? 'Liked' : 'Like'}</button></td>
-            `;
-        } else {
-            songRow.innerHTML = `
-                <td>${song.title}</td>
-                <td><button onclick="playSongFromList(${index})">Play</button></td>
-                <td><button onclick="addToPlaylist(${index})">Add to Playlist</button></td>
-                <td><button onclick="addToQueue(${index})">Add to Queue</button></td>
                 <td><button onclick="likeSong(${index})">${song.liked ? 'Liked' : 'Like'}</button></td>
-            `;
-        }
-        songList.appendChild(songRow);
+            </tr>
+        `;
+        songList.innerHTML += songRow;
     });
+}
+
+function addToPlaylist(index) {
+    currentPlaylist.push(songs[index]);
+    populatePlaylist();
+}
+
+function populatePlaylist() {
+    const playlistList = document.getElementById('playlistList');
+    playlistList.innerHTML = '';
+    currentPlaylist.forEach((song, index) => {
+        const songRow = `
+            <tr>
+                <td>${song.artist}</td>
+                <td>${song.songTitle}</td>
+                <td><button onclick="playSongFromList(${index})">Play</button></td>
+                <td><button onclick="removeFromPlaylist(${index})">Remove</button></td>
+                <td><button onclick="addToQueue(${index})">Add to Queue</button></td>
+                <td><button onclick="likeSong(${index})">Like</button></td>
+            </tr>
+        `;
+        playlistList.innerHTML += songRow;
+    });
+}
+
+function removeFromPlaylist(index) {
+    currentPlaylist.splice(index, 1);
+    populatePlaylist();
+}
+
+function likeSong(index) {
+    songs[index].liked = !songs[index].liked;
+    populateSongs();
+}
+
+function addToQueue(index) {
+    // Logic for adding song to queue
+    console.log(`Song ${index + 1} added to Queue`);
 }
 
 fetch('l4.json')
@@ -88,3 +110,8 @@ fetch('l4.json')
     .catch(error => {
         console.error("Error fetching songs from l4.json:", error);
     });
+
+// On page load
+document.addEventListener('DOMContentLoaded', function() {
+    populateSongs();
+});
