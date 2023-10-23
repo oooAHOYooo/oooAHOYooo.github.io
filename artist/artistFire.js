34dbcd18dd555d03b5d84956a1b24ef2f5a9cdae
agSuperCoder123 <alex@ahoy.ooo>
2023-10-23 12:30:34 -0400

    changed the default css for artists - added scripts so it can connect to the firebase sitch

---
+++ artist/artistFire.js
@@ -0,0 +1,30 @@
+		// TODO: Replace with your Firebase project's configuration
+		var firebaseConfig = { 
+    apiKey: "AIzaSyBq_IQRwkjh2o-jUkS8tI8wcEek5yMIwmo", 
+    authDomain: "ahoy-indie-media-da86d.firebaseapp.com", 
+    projectId: "ahoy-indie-media-da86d", 
+    storageBucket: "ahoy-indie-media-da86d.appspot.com", 
+    messagingSenderId: "179901301547", 
+    appId: "1:179901301547:web:599159f9efb826f464bb6e", 
+    measurementId: "G-K3QN161BVX", 
+  }; 
+  firebase.initializeApp(firebaseConfig);
+
+firebase.auth().onAuthStateChanged((user) => {
+	const userAccountDiv = document.getElementById("user-account");
+	const supportArtistDiv = document.getElementById("supportArtist");
+	if (user) {
+		userAccountDiv.innerHTML = `
+			<h2>Your User Account</h2>
+			<p>User: ${user.email}</p>
+			<p>Balance: $100</p>
+		`;
+		supportArtistDiv.innerHTML = '<a href="../../../pay/index.html">Tip Artist</a>';
+	} else {
+		userAccountDiv.innerHTML = `
+			Please log in to view your account information.
+			<button onclick="location.href='../../../account/loginModule.html';">Login</button>
+		`;
+		supportArtistDiv.innerHTML = '';
+	}
+});
