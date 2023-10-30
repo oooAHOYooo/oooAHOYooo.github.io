2a5721b965cbe89f22c5c1ee07a0da769316df64
agBabbbyCakes (ApeWorX, LTD) <alex@apeworx.io>
2023-10-30 15:59:36 -0400

    added the play button to the featured playlist button

---
+++ library/feature_featureSongTable.js
@@ -0,0 +1,36 @@
+// Fetch the JSON data
+fetch('l5.json')
+    .then(response => response.json())
+    .then(data => {
+        // Filter the songs where "featured" is true
+        const featuredSongs = data.songs.filter(song => song.featured);
+
+        // Get the table body
+        const tableBody = document.getElementById('featuredSongs');
+
+        // Loop through the featured songs and create table rows
+        featuredSongs.forEach(song => {
+            const row = document.createElement('tr');
+
+            row.innerHTML = `
+                <td>${song.artist}</td>
+                <td>${song.songTitle}</td>
+                <td><button class="tablePlayButton" onclick="playSongAndAddToQueue('${song.mp3url}', '${song.id}')">Play</button></td>
+            `;
+
+            // Append the row to the table body
+            tableBody.appendChild(row);
+        });
+    });
+ 
+// Function to play the song and add it to the top of the queue
+function playSongAndAddToQueue(mp3url, id) {
+    // Play the song
+    const audioPlayer = new Audio(mp3url);
+    audioPlayer.loop = false; // Ensure the audio only plays once
+    audioPlayer.play();
+
+    // Add the song to the top of the queue
+    queue.unshift({mp3url, id});
+}
+
