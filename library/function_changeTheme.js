574320da0b43ff16aec2340c57c14b58415f59d2
agSuperCoder123 <alex@ahoy.ooo>
2023-11-05 07:45:50 -0500

    hot fix on the change theme colors - and also made the player colors a bit more dynamic

--- library/function_changeTheme.js
+++ library/function_changeTheme.js
@@ -2,10 +2,10 @@ function changeTheme(theme) {
     var root = document.documentElement;
 
     if (theme === 'dark') {
-        root.style.setProperty('--background-color', '#000026');
-        root.style.setProperty('--text-color', '#ffffff');
+        root.style.setProperty('--background-color', 'rgb(0, 0, 19)');
+        root.style.setProperty('--text-color', '#0fb3ffba');
         root.style.setProperty('--border-color', '#006aff');
-        root.style.setProperty('--secondary-color', '#9e945c63');
+        root.style.setProperty('--secondary-color', '#0fb3ffba');
         root.style.setProperty('--button-color', '#5d5dff2d');
     } else if (theme === 'white') {
         root.style.setProperty('--background-color', '#ffffff');
