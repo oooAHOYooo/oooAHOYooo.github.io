d9d7bbb92dcdac6410687f3542eda7c60f1d0a2a
agSuperCoder123 <alex@ahoy.ooo>
2023-11-21 09:23:01 -0500

    added the ellie theme

--- cleanOy/src/function-switchTheme.js
+++ cleanOy/src/function-switchTheme.js
@@ -2,33 +2,34 @@ let theme = 'light';
 
 function switchTheme() {
     const root = document.documentElement;
+    const themeTitle = document.getElementById('theme-title');
 
     if (theme === 'light') {
-        // If the current theme is light, switch to dark
-        root.style.setProperty('--background-color', 'rgb(0, 14, 19)');
-        root.style.setProperty('--text-color', '#00f2ff');
-        root.style.setProperty('--border-color', '#00e5ff');
-        root.style.setProperty('--secondary-color', '#000000ba');
-        root.style.setProperty('--button-color', '#ffffff2d');
-        root.style.setProperty('--gold', '#ffd900');
+        setThemeProperties(root, 'rgb(0, 14, 19)', '#00f2ff', '#00e5ff', '#000000ba', '#ffffff2d', '#ffd900');
         theme = 'dark';
     } else if (theme === 'dark') {
-        // If the current theme is dark, switch to bright pink
-        root.style.setProperty('--background-color', '#ff69be');
-        root.style.setProperty('--text-color', '#ffffff');
-        root.style.setProperty('--border-color', '#ff1493');
-        root.style.setProperty('--secondary-color', '#ff69b4');
-        root.style.setProperty('--button-color', '#ff1493');
-        root.style.setProperty('--gold', '#ffd900');
+        setThemeProperties(root, '#ff69be', '#ffffff', '#ff1493', '#ff69b4', '#ff1493', '#ffd900');
         theme = 'pink';
+    } else if (theme === 'pink') {
+        setThemeProperties(root, '#98ff98', 'rgb(0, 119, 97)', '#3eb489', '#98ff98', '#3eb489', '#ffd900');
+        theme = 'ellie';
     } else {
-        // If the current theme is pink, switch to light
-        root.style.setProperty('--background-color', '#ffffff');
-        root.style.setProperty('--text-color', '#000000');
-        root.style.setProperty('--border-color', '#e0e0e0');
-        root.style.setProperty('--secondary-color', '#f2f2f2');
-        root.style.setProperty('--button-color', '#d9d9d9');
-        root.style.setProperty('--gold', '#ffd900');
+        setThemeProperties(root, '#ffffff', '#000000', '#e0e0e0', '#f2f2f2', '#d9d9d9', '#ffd900');
         theme = 'light';
     }
+
+    themeTitle.innerText = theme;
+    themeTitle.classList.add('fade');
+    setTimeout(() => themeTitle.classList.remove('fade'), 2000);
 }
+
+function setThemeProperties(root, backgroundColor, textColor, borderColor, secondaryColor, buttonColor, goldColor) {
+    root.style.setProperty('--background-color', backgroundColor);
+    root.style.setProperty('--text-color', textColor);
+    root.style.setProperty('--border-color', borderColor);
+    root.style.setProperty('--secondary-color', secondaryColor);
+    root.style.setProperty('--button-color', buttonColor);
+    root.style.setProperty('--gold', goldColor);
+}
+
+
