947abb0db787c3f08da370868c43728515820a56
agBabbbyCakes (ApeWorX, LTD) <alex@apeworx.io>
2023-11-22 08:17:52 -0500

    attempted to update the update tab

--- cleanOy/src/updates-commits.js
+++ cleanOy/src/updates-commits.js
@@ -1,28 +1,29 @@
-        function loadCommits() {
-            fetch('https://api.github.com/repos/oooAHOYooo/oooAHOYooo.github.io/commits')
-                .then(response => response.json())
-                .then(commits => {
-                    const commitsList = document.getElementById('commits-list');
-                    commitsList.innerHTML = ''; // Clear the list
+function loadCommits() {
+    fetch('https://api.github.com/repos/oooAHOYooo/oooAHOYooo.github.io/commits')
+        .then(response => response.json())
+        .then(commits => {
+            const commitsList = document.getElementById('commits-list');
+            commitsList.innerHTML = ''; // Clear the list
 
-                    commits.forEach(commit => {
-                        const commitItem = document.createElement('div');
-                        commitItem.className = 'commit-item';
+            commits.forEach(commit => {
+                const commitItem = document.createElement('div');
+                commitItem.className = 'commit-item';
 
-                        const date = new Date(commit.commit.author.date);
-                        const formattedDate = `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}`;
+                const date = new Date(commit.commit.author.date);
+                const formattedDate = `${date.getHours()}:${date.getMinutes()}`;
 
-                        commitItem.innerHTML = `
-                        <h3>${commit.commit.message}</h3>
-                        <p>Author: ${commit.commit.author.name}</p>
-                        <p>Date: ${formattedDate}</p>
-                    `;
+                commitItem.innerHTML = `
+                <h3>${commit.commit.message}</h3>
+                <p>Author: ${commit.commit.author.name}</p>
+                <p>Time: ${formattedDate}</p>
+                <p>Description: ${commit.commit.message}</p>
+                `;
 
-                        commitsList.appendChild(commitItem);
-                    });
-                })
-                .catch(error => console.error('Error:', error));
-        }
+                commitsList.appendChild(commitItem);
+            });
+        })
+        .catch(error => console.error('Error:', error));
+}
 
-        // Load commits when the page loads
-        window.onload = loadCommits;
+// Load commits when the page loads
+window.onload = loadCommits;
