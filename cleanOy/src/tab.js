bf8b88b85400c28d7c7be49579c03060e6b7729e
ahoymrag <ahoy.mr.ag@gmail.com>
2023-12-01 11:31:40 -0500

    successfully added a home tab - little glitchy - but it now loads by default

--- cleanOy/src/tab.js
+++ cleanOy/src/tab.js
@@ -7,16 +7,13 @@ function switchTab(tabName) {
     }
     const activeTab = document.getElementById(`${tabName}-tab`);
     activeTab.classList.add('active');
-
-
 }
-
 
 let currentTabIndex = 0; // Assuming you have a global variable to keep track of the current tab
 
 function nextTab() {
     currentTabIndex++;
-    if (currentTabIndex > 10) { // Reset to the first tab if we've gone past the last one
+    if (currentTabIndex > 12) { // Reset to the first tab if we've gone past the last one
         currentTabIndex = 0;
     }
     switchTabByIndex(currentTabIndex);
@@ -25,14 +22,14 @@ function nextTab() {
 function prevTab() {
     currentTabIndex--;
     if (currentTabIndex < 0) { // Loop back to the last tab if we've gone past the first one
-        currentTabIndex = 10;
+        currentTabIndex = 12;
     }
     switchTabByIndex(currentTabIndex);
 }
 
 // Extend the existing switchTab function to accept an index
 function switchTabByIndex(tabIndex) {
-    const tabs = ['nowplaying', 'radio', 'songs', 'liked', 'podcasts', 'artists', 'cable', 'updates', 'games', 'media', 'account'];
+    const tabs = ['home', 'nowplaying', 'radio', 'songs', 'liked', 'podcasts', 'artists', 'cable', 'updates', 'games', 'media', 'account', 'nowplaying'];
     const tabIndicator = document.getElementById('tab-indicator');
     if (tabIndex >= 0 && tabIndex < tabs.length) {
         switchTab(tabs[tabIndex]);
@@ -40,4 +37,3 @@ function switchTabByIndex(tabIndex) {
         tabIndicator.textContent = tabs[tabIndex].charAt(0).toUpperCase() + tabs[tabIndex].slice(1); // Capitalize the first letter
     }
 }
-
