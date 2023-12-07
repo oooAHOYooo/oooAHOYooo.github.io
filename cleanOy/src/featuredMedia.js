e49550c7b13bb678323cdf4f75abb7345934591c
agSuperCoder123 <alex@ahoy.ooo>
2023-12-06 20:14:53 -0500

    successfully parsed out the featured media javascript into its own thing

---
+++ cleanOy/src/featuredMedia.js
@@ -0,0 +1,48 @@
+// Function to get 'n' random elements from an array
+function getRandom(arr, n) {
+  const result = new Array(n);
+  let len = arr.length;
+  const taken = new Array(len);
+
+  if (n > len) {
+    throw new RangeError("getRandom: more elements taken than available");
+  }
+
+  while (n--) {
+    const x = Math.floor(Math.random() * len);
+    result[n] = arr[x in taken ? taken[x] : x];
+    taken[x] = --len in taken ? taken[len] : len;
+  }
+
+  return result;
+}
+
+// Function to load and display featured media
+function loadFeaturedMedia() {
+  fetch("data/featuredMedia.json") // Make sure the path is correct
+    .then((response) => response.json())
+    .then((data) => {
+      const container = document.getElementById("featured-media");
+      container.innerHTML = ""; // Clear previous content
+
+      // Get 3 random items
+      const randomData = getRandom(data, 3);
+
+      // Add the random items to the DOM
+      randomData.forEach((item) => {
+        const mediaItem = document.createElement("div");
+        mediaItem.className = "media-item";
+        mediaItem.innerHTML = `
+                    <div class="media-img">
+                        <img src="${item.imgSrc}" alt="${item.altText}">
+                        <p class="media-description">${item.description}</p>
+                    </div>
+                `;
+        container.appendChild(mediaItem);
+      });
+    })
+    .catch((error) => console.error("Failed to load featured media:", error));
+}
+
+// Execute when the DOM is fully loaded
+document.addEventListener("DOMContentLoaded", loadFeaturedMedia);
