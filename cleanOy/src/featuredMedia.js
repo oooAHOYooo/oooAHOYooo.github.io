2ba4943ddb984bec66aa0bfef61c2be93248a460
agSuperCoder123 <alex@ahoy.ooo>
2023-12-08 11:24:33 -0500

    made it so - just like the thumbnail on the podcast page - the featured media artists now get a popup

--- cleanOy/src/featuredMedia.js
+++ cleanOy/src/featuredMedia.js
@@ -33,12 +33,47 @@ function loadFeaturedMedia() {
         const mediaItem = document.createElement("div");
         mediaItem.className = "media-item";
         mediaItem.innerHTML = `
-                    <div class="media-img">
-                        <img src="${item.imgSrc}" alt="${item.altText}">
-                        <p class="media-description">${item.description}</p>
-                    </div>
-                `;
+                      <div class="media-img">
+                          <img src="${item.imgSrc}" alt="${item.altText}" class="thumbnail">
+                          <p class="media-description">${item.description}</p>
+                      </div>
+                  `;
         container.appendChild(mediaItem);
+      });
+
+      // Add event listener to thumbnails
+      const thumbnails = document.querySelectorAll(".thumbnail");
+      thumbnails.forEach((thumbnail) => {
+        thumbnail.addEventListener("click", function () {
+          // Create a lightbox
+          const lightbox = document.createElement("div");
+          lightbox.style.position = "relative";
+          lightbox.style.top = "0";
+          lightbox.style.right = "0";
+          lightbox.style.bottom = "0";
+          lightbox.style.left = "0";
+          lightbox.style.backgroundColor = "rgba(0, 0, 0, 0.7)";
+          lightbox.style.display = "flex";
+          lightbox.style.justifyContent = "center";
+          lightbox.style.alignItems = "center";
+          lightbox.style.zIndex = "1000";
+
+          // Show the full media cover
+          const fullCover = new Image();
+          fullCover.src = this.src;
+          fullCover.alt = this.alt;
+          fullCover.style.width = "100%";
+          fullCover.style.height = "auto";
+          fullCover.style.maxWidth = "80%";
+
+          lightbox.appendChild(fullCover);
+          document.body.appendChild(lightbox);
+
+          // Remove the lightbox when clicked
+          lightbox.addEventListener("click", function () {
+            document.body.removeChild(lightbox);
+          });
+        });
       });
     })
     .catch((error) => console.error("Failed to load featured media:", error));
