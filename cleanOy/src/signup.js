0b05e98b0f84c11618dd29f4df0eeb24a6cc4b86
agSuperCoder123 <alex@ahoy.ooo>
2023-12-14 10:18:36 -0500

    updated some more supabase javascript - hope it works

--- cleanOy/src/signup.js
+++ cleanOy/src/signup.js
@@ -1,30 +1,37 @@
-// Initialize Supabase
-const supabase = supabase.createClient(
-  "YOUR_SUPABASE_URL",
-  "YOUR_SUPABASE_ANON_KEY"
-);
+// Initialize Supabase client
+const supabaseUrl = "https://ornjxycoizoybvdhhowp.supabase.co";
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9ybmp4eWNvaXpveWJ2ZGhob3dwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDIzNTA4ODEsImV4cCI6MjAxNzkyNjg4MX0.SIgYEybVeKQ5xGnplA-elTOw0oC5tZIv5nGRUZf-vS8";
+const supabase = Supabase.createClient(supabaseUrl, supabaseAnonKey);
 
-document
-  .getElementById("signup-form")
-  .addEventListener("submit", async function (e) {
-    e.preventDefault();
+document.getElementById("signup-form").addEventListener("submit", async (e) => {
+  e.preventDefault();
 
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-    const plan = document.querySelector('input[name="plan"]:checked').value;
+  // Retrieve user input
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const plan = document.querySelector('input[name="plan"]:checked').value;
 
-    try {
-      const { user, error } = await supabase.auth.signUp({ email, password });
+  // Optional: Handling different plans (expand upon this as needed)
+  console.log(`Selected Plan: ${plan}`);
 
-      if (error) throw error;
+  try {
+    // Attempt to create a new user
+    const { user, error } = await supabase.auth.signUp({
+      email,
+      password,
+    });
 
-      // Handle the chosen plan here
-      // For example, you might want to store this information in your database
-      console.log(`User signed up with ${plan} plan`);
+    if (error) throw error;
 
-      // Redirect to a different page or show a success message
-    } catch (error) {
-      console.error("Error during sign-up:", error);
-      // Show error message to the user
-    }
-  });
+    // Display a message indicating the user can close the page
+    alert(
+      "Signup successful! Please check your email for a confirmation link. You can now close this page."
+    );
+
+    // Optionally, remove the redirect to keep the user on the page
+    // window.location.href = 'login.html';
+  } catch (error) {
+    alert(`Signup failed: ${error.message}`);
+  }
+});
