4bbc27cd4a3562401dbf7a4de50e56871e952080
agSuperCoder123 <alex@ahoy.ooo>
2023-12-15 01:02:03 -0500

    big crunch late night - made a bunch of artist pages - made them so they work in the index of the app - as an iframe - and now trying to get there songs to play on the index but will have to push - not working on locally - tired - gonna call it - didnt get as far as i wanted to with login but tomorrow is another day

--- cleanOy/src/artists/artistData.js
+++ cleanOy/src/artists/artistData.js
@@ -22,4 +22,81 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     })
     .catch((error) => console.error("Error loading artist data:", error));
+
+  // Fetch song data and create a table
+  fetch("../../data/songCollection.json")
+    .then((response) => response.json())
+    .then((data) => {
+      const songs = data.songs.filter((song) => song.artist === artistName);
+      if (songs.length > 0) {
+        const table = document.createElement("table");
+        table.className = "song-table"; // Add this line
+        songs.forEach((song) => {
+          const row = document.createElement("tr");
+
+          // Song title cell
+          const titleCell = document.createElement("td");
+          titleCell.textContent = song.songTitle;
+          row.appendChild(titleCell);
+
+          // Play button cell
+          const playCell = document.createElement("td");
+          const playButton = document.createElement("button");
+          playButton.textContent = "Play";
+          playButton.onclick = function () {
+            // Code to play the song
+            const audio = new Audio(song.mp3url);
+            audio.play();
+          };
+          playCell.appendChild(playButton);
+          row.appendChild(playCell);
+
+          // Buy button cell
+          const buyCell = document.createElement("td");
+          const buyButton = document.createElement("button");
+          buyButton.textContent = "Buy";
+          buyButton.onclick = function () {
+            // Code to buy the song
+            window.open(song.artistUrl, "_blank");
+          };
+          buyCell.appendChild(buyButton);
+          row.appendChild(buyCell);
+
+          table.appendChild(row);
+        });
+        document.body.appendChild(table);
+      }
+    })
+    .catch((error) => console.error("Error loading song data:", error));
 });
+
+const playSong = function () {
+  const url = this.dataset.url;
+  const artist = this.dataset.artist;
+  const title = this.dataset.title;
+  const playButtons = document.querySelectorAll(".play-button");
+  const bottomPlayButton = document.querySelector("#play");
+  const bottomPlayIcon = bottomPlayButton.querySelector("i");
+
+  if (audioPlayer.src !== url) {
+    audioPlayer.src = url;
+  }
+
+  if (audioPlayer.paused) {
+    audioPlayer.play();
+    playButtons.forEach((button) => {
+      if (button.dataset.url === url) {
+        button.querySelector("i").className = "fas fa-pause";
+      }
+    });
+    bottomPlayIcon.className = "fas fa-pause";
+  } else {
+    audioPlayer.pause();
+    playButtons.forEach((button) => {
+      if (button.dataset.url === url) {
+        button.querySelector("i").className = "fas fa-play";
+      }
+    });
+    bottomPlayIcon.className = "fas fa-play";
+  }
+};
