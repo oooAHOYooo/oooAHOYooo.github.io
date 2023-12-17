6cda797a505215437bf486637b3456764a40ba68
agBabbbyCakes (ApeWorX, LTD) <alex@apeworx.io>
2023-12-17 08:59:02 -0500

    updated the format and vibes of the update page a bit

--- cleanOy/src/updates-commits.js
+++ cleanOy/src/updates-commits.js
@@ -1,5 +1,5 @@
 function loadCommits() {
-    fetch('https://api.github.com/repos/oooAHOYooo/oooAHOYooo.github.io/commits')
+    fetch('https://api.github.com/repos/oooAHOYooo/oooAHOYooo.github.io/commits?per_page=50')
         .then(response => response.json())
         .then(commits => {
             const commitsList = document.getElementById('commits-list');
@@ -10,13 +10,13 @@ function loadCommits() {
                 commitItem.className = 'commit-item';
 
                 const date = new Date(commit.commit.author.date);
-                const formattedDate = `${date.getHours()}:${date.getMinutes()}`;
+                const formattedDate = `${date.toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric' })} - ${date.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', hour12: true })}`;
 
                 commitItem.innerHTML = `
                 <h3>${commit.commit.message}</h3>
                 <p>Author: ${commit.commit.author.name}</p>
                 <p>Time: ${formattedDate}</p>
-                <p>Description: ${commit.commit.message}</p>
+        
                 `;
 
                 commitsList.appendChild(commitItem);
