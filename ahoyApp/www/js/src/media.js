709e7c8b44848e86159562b987e96a44e59197d5
agBabbbyCakes (ApeWorX, LTD) <alex@apeworx.io>
2023-12-21 19:13:58 -0500

    made it so that it the media loads at random

--- ahoyApp/www/js/src/media.js
+++ ahoyApp/www/js/src/media.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   // Function to load video in JW Player
   function loadVideoInJWPlayer(videoUrl, thumbnailUrl) {
     jwplayer("jw-player-container").setup({
-      autostart: true,
+      autostart: false, // Autoplay disabled
       file: videoUrl,
       image: thumbnailUrl,
       width: "100%",
@@ -10,10 +10,20 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 
+  // Function to select a random video
+  function selectRandomVideo(videos) {
+    const randomIndex = Math.floor(Math.random() * videos.length);
+    return videos[randomIndex];
+  }
+
   // Fetch and display media data
   fetch("data/mediaCollection.json")
     .then((response) => response.json())
     .then((data) => {
+      // Select and load a random video
+      const randomVideo = selectRandomVideo(data);
+      loadVideoInJWPlayer(randomVideo.mp4_link, randomVideo.thumbnail_link);
+
       const tableBody = document.querySelector("#mediaTable tbody");
 
       data.forEach((item) => {
