cc9948f215445d9fef28b38c130e97e21c3064df
agSuperCoder123 <alex@ahoy.ooo>
2023-12-23 08:44:42 -0500

    changed up the game

--- ahoyApp/www/js/src/games.js
+++ ahoyApp/www/js/src/games.js
@@ -1,34 +1,78 @@
-    document.getElementById('load-game-a').addEventListener('click', function() {
-    this.style.display = 'none';
-
-    
+document.addEventListener("DOMContentLoaded", function () {
+  loadGames();
 });
 
-document.getElementById('load-game-b').addEventListener('click', function() {
-    this.style.display = 'none';
+function loadGames() {
+  // Example JSON structure. Replace this with the fetch from your actual JSON file
+  const games = [
+    {
+      id: "GameA",
+      name: "Vanca",
+      url: "https://marcoworms.github.io/vanca/",
+      description: "Game by Marco Worms",
+      infoUrl: "https://marcoworms.github.io/",
+    },
+    {
+      id: "GameB",
+      name: "Attach",
+      url: "https://marcoworms.github.io/attach/",
+      description: "Game by Marco Worms",
+      infoUrl: "https://marcoworms.github.io/",
+    },
+    // Add more games as needed
+  ];
 
-    
-});
-function openGame(evt, gameName) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(gameName).style.display = "block";
-    evt.currentTarget.className += " active";
+  createGameTabs(games);
 }
 
-document.getElementById('load-game-a').addEventListener('click', function() {
-    document.getElementById('game-frame-a').src = "https://marcoworms.github.io/vanca/";
-});
+function createGameTabs(games) {
+  const tabsContainer = document.querySelector(".game-tabs"); // Updated class name
+  const tabContentContainer = document.getElementById("games-tab");
 
-document.getElementById('load-game-b').addEventListener('click', function() {
-    document.getElementById('game-frame-b').src = "https://marcoworms.github.io/attach/";
-});
+  games.forEach((game) => {
+    // Create tab button
+    const tabButton = document.createElement("button");
+    tabButton.className = "game-tablinks"; // Updated class name
+    tabButton.textContent = game.name;
+    tabButton.onclick = function (event) {
+      openGame(event, game.id);
+    };
+    tabsContainer.appendChild(tabButton);
 
-// Add similar event listeners for other games
+    // Create tab content
+    const tabContent = document.createElement("div");
+    tabContent.id = game.id;
+    tabContent.className = "game-tabcontent"; // Updated class name
+    tabContent.style.display = "none"; // Start with hidden content
+    tabContent.innerHTML = `
+            <button class="load-game" data-url="${game.url}">Load "${game.name}"</button>
+            <iframe style="width: 100%; height: 500px; border: none"></iframe>
+            <h1>${game.name}</h1>
+            <h2>${game.description}</h2>
+            <p>For more information about this game, visit <a href="${game.infoUrl}" target="_blank">here</a>.</p>
+        `;
+    tabContentContainer.appendChild(tabContent);
+
+    // Add event listener for load button
+    tabContent
+      .querySelector(".load-game")
+      .addEventListener("click", function () {
+        this.nextElementSibling.src = this.getAttribute("data-url");
+        this.style.display = "none";
+      });
+  });
+}
+
+function openGame(evt, gameName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("game-tabcontent"); // Updated class name
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("game-tablinks"); // Updated class name
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(gameName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
