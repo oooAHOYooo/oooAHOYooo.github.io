facde91fde13184ddd173b1cc7af142a886f6674
agSuperCoder123 <alex@ahoy.ooo>
2024-02-06 10:17:00 -0500

    added basic left dash - at least the buttons with the functions

---
+++ ahoyApp/www/js/src/nav-leftDash.js
@@ -0,0 +1,8 @@
+    function navigateToTab(tabId) {
+      // Hide all tabs
+      document.querySelectorAll('.tab-content').forEach(tab => {
+        tab.style.display = 'none';
+      });
+      // Show the selected tab
+      document.getElementById(tabId).style.display = 'block';
+    }
