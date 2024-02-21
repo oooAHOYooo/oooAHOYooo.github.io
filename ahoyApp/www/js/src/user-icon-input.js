dd204b10002b745bd75712d08a9b4e45c5de4d5c
agSuperCoder123 <alex@ahoy.ooo>
2024-02-21 14:45:53 -0500

    updated the user icon sugar

--- ahoyApp/www/js/src/user-icon-input.js
+++ ahoyApp/www/js/src/user-icon-input.js
@@ -1,10 +1,13 @@
-    document.getElementById('user-icon-input').addEventListener('change', function(event) {
-      const file = event.target.files[0];
-      if (file) {
-        const reader = new FileReader();
-        reader.onload = function(e) {
-          document.getElementById('user-icon').src = e.target.result;
-        };
-        reader.readAsDataURL(file);
-      }
-    });
+document.getElementById('user-icon-input').addEventListener('change', function(event) {
+  const file = event.target.files[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = function(e) {
+      // Set the src attribute for the user icon
+      document.getElementById('user-icon').src = e.target.result;
+      // Also set the background image of the tab
+      document.body.style.backgroundImage = `url('${e.target.result}')`;
+    };
+    reader.readAsDataURL(file);
+  }
+});
