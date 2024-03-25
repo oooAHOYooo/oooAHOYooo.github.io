32c0e548a33305642956f1eabdb5629b104cedda
agSuperCoder123 <alex@ahoy.ooo>
2024-03-25 16:05:20 -0400

    did some open heart surgery on the account section

--- ahoyApp/www/js/src/user-liked-everything.js
+++ ahoyApp/www/js/src/user-liked-everything.js
@@ -19,3 +19,14 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const player = document.getElementById('audio-player');
+    
+    document.querySelectorAll('.play-button').forEach(button => {
+      button.addEventListener('click', function() {
+        player.src = this.getAttribute('data-track');
+        player.play();
+      });
+    });
+  });
