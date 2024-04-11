48b82508824f7ccd6437db3c4e9e3f010fc566a4
agSuperCoder123 <alex@ahoy.ooo>
2024-04-11 13:45:07 -0400

    broke the site because of css ids - need to make unique ids htroughout

--- ahoyApp/www/js/src/newSuperSong.js
+++ ahoyApp/www/js/src/newSuperSong.js
@@ -24,8 +24,12 @@ function loadSongs() {
         const row = document.createElement("tr");
         row.className = 'table-row'; // Add class for styling
         row.innerHTML = `
-          <td><button class="control-button-a" onclick="playSong('${song.mp3url}', '${song.songTitle}', '${song.artist}', this)"><i class="fas fa-play"></i></button></td>
-          <td><img src="${song.coverArt}" alt="${song.songTitle}" class="thumbnail"></td>
+          <td class="image-and-play-button">
+            <img src="${song.coverArt}" alt="${song.songTitle}" class="thumbnail">
+            <button class="play-button-overlay" onclick="playSong('${song.mp3url}', '${song.songTitle}', '${song.artist}', this)">
+              <i class="fas fa-play"></i>
+            </button>
+          </td>
           <td>${song.artist}</td>
           <td>${song.songTitle}</td>
           <td><button class="control-button-b" onclick="burnSong('${song.mp3url}', '${song.songTitle}', '${song.artist}', this)"><i class="fas fa-fire"></i></button></td>
