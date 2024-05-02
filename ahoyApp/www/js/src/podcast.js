783b7b8fb737db5a0cae931b7497bd9e19721a52
agSuperCoder123 <alex@ahoy.ooo>
2024-05-01 23:26:53 -0400

    added podcast

--- ahoyApp/www/js/src/podcast.js
+++ ahoyApp/www/js/src/podcast.js
@@ -1,22 +1,35 @@
-// Function to load and display podcasts from podcastCollection.json
+// Function to load and display podcasts from jsonbin.io
 function loadPodcasts() {
-  fetch("data/podcastCollection.json")
-    .then(response => response.json())
-    .then(data => {
-      const tableBody = document.getElementById("podcast-table").querySelector("tbody");
+    const url = "https://api.jsonbin.io/v3/b/662f022dacd3cb34a8400e3e";
+    const accessKey = '$2a$10$4GRuokwTdv4sRnqIwYDyGOWZ2CZgDkefsKy7OFlmTydfnDvXBomtC'; // Use your read-only access key here
 
-      data.podcasts.forEach((podcast) => {
-        const row = document.createElement("tr");
-        row.innerHTML = `
-          <td><button class="control-button-podcast" id="podcast-play-${podcast.id}" onclick="togglePlayPausePodcast('${podcast.mp3url}', '${podcast.thumbnail}', '${podcast.title}', '${podcast.description}', ${podcast.id})"><i class="fas fa-play"></i></button></td>
-          <td><img src="${podcast.thumbnail}" alt="${podcast.title}" class="thumbnail"></td>
-          <td>${podcast.title}</td>
-          <td>${podcast.description}</td>
-        `;
-        tableBody.appendChild(row);
-      });
+    fetch(url, {
+        headers: {
+            'X-Access-Key': accessKey
+        }
     })
-    .catch(error => console.error("Error loading podcasts:", error));
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        const tableBody = document.getElementById("podcast-table").querySelector("tbody");
+        data.record.podcasts.forEach(podcast => {
+            const row = document.createElement("tr");
+            row.innerHTML = `
+                <td><button class="control-button-podcast" id="podcast-play-${podcast.id}" onclick="togglePlayPausePodcast('${podcast.mp3url}', '${podcast.thumbnail}', '${podcast.title}', '${podcast.description}', ${podcast.id})"><i class="fas fa-play"></i></button></td>
+                <td><img src="${podcast.thumbnail}" alt="${podcast.title}" class="thumbnail"></td>
+                <td>${podcast.title}</td>
+                <td>${podcast.description}</td>
+            `;
+            tableBody.appendChild(row);
+        });
+    })
+    .catch(error => {
+        console.error("Error loading podcasts:", error);
+    });
 }
 
 // Adjust the togglePlayPausePodcast function to use podcast.id instead of index
