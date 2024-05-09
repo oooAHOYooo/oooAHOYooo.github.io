8da62635e5837e3bbe122a4e00d3091d798d0a8b
agSuperCoder123 <alex@ahoy.ooo>
2024-05-08 23:08:23 -0400

    switched the order of the table in the classical music mode

--- ahoyApp/www/js/src/classical_music.js
+++ ahoyApp/www/js/src/classical_music.js
@@ -4,28 +4,26 @@ async function loadClassicalMusic() {
     const table = document.createElement('table');
     table.innerHTML = `
         <tr>
+            <th>Play</th>
             <th>Title</th>
             <th>Composer</th>
             <th>Length</th>
             <th>Year</th>
             <th>Genre</th>
-            <th>Album</th>
-            <th>Movements</th>
-            <th>Play</th>
+            <th>Performed By</th>
         </tr>
     `;
 
     musicList.forEach(music => {
         const row = document.createElement('tr');
         row.innerHTML = `
+            <td><button onclick="playMusic('${music.mp3_url}')"><i class="fas fa-play"></i></button></td>
             <td>${music.title}</td>
             <td>${music.composer}</td>
             <td>${music.length}</td>
             <td>${music.year}</td>
             <td>${music.genre}</td>
-            <td>${music.album}</td>
-            <td>${music.movements ? music.movements.join(', ') : 'N/A'}</td>
-            <td><button onclick="playMusic('${music.mp3_url}')">Play</button></td>
+            <td>${music.performed_by}</td>
         `;
         table.appendChild(row);
     });
