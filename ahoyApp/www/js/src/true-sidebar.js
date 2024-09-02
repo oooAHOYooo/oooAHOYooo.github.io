7674904e638c476bcb06bc6acdc2b9caf7216cef
agSuperCoder123 <alex@ahoy.ooo>
2024-09-01 23:27:09 -0400

    last update push

--- ahoyApp/www/js/src/true-sidebar.js
+++ ahoyApp/www/js/src/true-sidebar.js
@@ -1,32 +1,32 @@
-  document.addEventListener('DOMContentLoaded', function() {
-        function toggleSidebars() {
-          var sidedock = document.getElementById('sidedock');
-          var rightSidebar = document.getElementById('right-sidebar');
-          if (window.innerWidth > 1024) {
-            sidedock.style.left = '0px';
-            rightSidebar.style.display = 'block';
-          } else {
-            sidedock.style.left = '-100%';
-            rightSidebar.style.display = 'none';
-          }
-        }
+document.addEventListener('DOMContentLoaded', function() {
+  function toggleSidebars() {
+    var sidedock = document.getElementById('sidedock');
+    var rightSidebar = document.getElementById('right-sidebar');
+    if (window.innerWidth > 1024) {
+      sidedock.style.left = '0px';
+      rightSidebar.style.display = 'block';
+    } else {
+      sidedock.style.left = '-100%';
+      rightSidebar.style.display = 'none';
+    }
+  }
 
-        toggleSidebars();
-        window.addEventListener('resize', toggleSidebars);
+  toggleSidebars();
+  window.addEventListener('resize', toggleSidebars);
 
-        document.getElementById('toggle-button').addEventListener('click', function() {
-          if (sidedock.style.left === '0px') {
-            sidedock.style.left = '-100%'; // Hide sidebar
-          } else {
-            sidedock.style.left = '0px'; // Show sidebar
-          }
-        });
+  document.getElementById('toggle-button').addEventListener('click', function() {
+    if (sidedock.style.left === '0px') {
+      sidedock.style.left = '-100%'; // Hide sidebar
+    } else {
+      sidedock.style.left = '0px'; // Show sidebar
+    }
+  });
 
-        document.getElementById('right-toggle-button').addEventListener('click', function() {
-          if (rightSidebar.style.display === 'block') {
-            rightSidebar.style.display = 'none'; // Hide sidebar
-          } else {
-            rightSidebar.style.display = 'block'; // Show sidebar
-          }
-        });
-      });
+  document.getElementById('right-toggle-button').addEventListener('click', function() {
+    if (rightSidebar.style.display === 'block') {
+      rightSidebar.style.display = 'none'; // Hide sidebar
+    } else {
+      rightSidebar.style.display = 'block'; // Show sidebar
+    }
+  });
+});
