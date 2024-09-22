990d0ba664714b7fa98a199dda60d2c1e5ed39f8
agSuperCoder123 <alex@ahoy.ooo>
2024-09-22 15:17:57 -0400

    trying trying to fix the podcast got the gradient default

--- ahoyApp/www/js/src/function-switchTheme.js
+++ ahoyApp/www/js/src/function-switchTheme.js
@@ -1,7 +1,7 @@
 const themes = ["light", "dark", "pink", "ellie", "bug", "hiii", "crazy", "gradientPink"]; // List of available themes
 
-// Initialize with a random theme from 'gradientPink', 'dark', or 'pink'
-let theme = ["gradientPink", "dark", "pink"][Math.floor(Math.random() * 3)];
+// Initialize with gradientPink as the default theme
+let theme = "gradientPink";
 
 function switchTheme(selectedTheme) {
   const root = document.documentElement;
