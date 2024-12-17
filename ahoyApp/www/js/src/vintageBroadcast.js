5e4d2956f287ef5068abf7267f4e405e10ee76de
agBabbbyCakes <108819253+agBabbbyCakes@users.noreply.github.com>
2024-12-17 07:56:09 -0500

    added mintea to the morning 1 json - switched the broadcast to show the josh caba on dec 20 onward

--- ahoyApp/www/js/src/vintageBroadcast.js
+++ ahoyApp/www/js/src/vintageBroadcast.js
@@ -66,7 +66,7 @@ async function loadPlaylistForTimeOfDay() {
     try {
         const now = new Date();
         const isThanksgiving = now.getMonth() === 10 && now.getDate() >= 28 && now.getDate() <= 30;
-        const isHolidaySpecial = now.getFullYear() === 2024 && now.getMonth() === 11 && now.getDate() >= 16 && now.getDate() <= 20;
+        const isHolidaySpecial = now.getFullYear() === 2024 && now.getMonth() === 11 && now.getDate() >= 20 && now.getDate() <= 30;
 
         if (isThanksgiving) {
             loadPlaylistFromJSON('./local_data/vintage-broadcast/special_playlists/palbot2_thanksgiving.json', "Pal-bot 2 Thanksgiving Marathon");
