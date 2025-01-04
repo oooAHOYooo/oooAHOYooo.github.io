46c46e350ea5631bb7435d0b29a62021253d5333
agBabbbyCakes <108819253+agBabbbyCakes@users.noreply.github.com>
2025-01-04 11:44:37 -0500

    have the login stuff on the core

---
+++ ahoyApp/www/js/src/core.js
@@ -0,0 +1,4 @@
+const auth0 = await createAuth0Client({
+    domain: 'dev-eytb1k2dke7distu.us.auth0.com', // Your Auth0 domain
+    client_id: 'IgWr7J24VQax8pAuTmohlQSSUIq8pNSc', // Your Client ID
+});
