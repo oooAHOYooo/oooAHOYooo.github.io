152924e288e5aa0a93b76b903e498a1c28087b9a
agBabbbyCakes <108819253+agBabbbyCakes@users.noreply.github.com>
2025-01-25 23:12:17 -0500

    updated logins

--- ahoyApp/www/js/fire/firebase-config.js
+++ ahoyApp/www/js/fire/firebase-config.js
@@ -8,4 +8,5 @@ const firebaseConfig = {
 };
 
 firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
 const db = firebase.firestore();
--- ahoyApp/www/js/fire/firestore.js
+++ ahoyApp/www/js/fire/firestore.js
@@ -1,3 +1,18 @@
+// Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBq_IQRwkjh2o-jUkS8tI8wcEek5yMIwmo",
+    authDomain: "ahoy-indie-media-da86d.firebaseapp.com",
+    projectId: "ahoy-indie-media-da86d",
+    storageBucket: "ahoy-indie-media-da86d.appspot.com",
+    messagingSenderId: "179901301547",
+    appId: "1:179901301547:web:599159f9efb826f464bb6e"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
+
 window.saveUserData = async function(username, email, plan) {
     try {
         const usernameCheck = await db.collection("users").where("username", "==", username).get();
